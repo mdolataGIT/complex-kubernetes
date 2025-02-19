7e7a8665b324a1d71d48d73d3dac58ffcd9996d1
Maciej <maciejdolata69@gmail.com>
2025-02-19 13:07:57 +0100

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,16 @@
-docker build -t maciej55/multi-client:latest -t maciej55/multi-client:$SHA ./client/Dockerfile ./client
-docker build -t maciej55/multi-server:latest -t maciej55/multi-server:$SHA ./server/Dockerfile ./server
-docker build -t maciej55/multi-worker:latest -t maciej55/multi-worker:$SHA ./worker/Dockerfile ./worker
+docker build -t maciej55/multi-client-k8s:latest -t maciej55/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t maciej55/multi-server-k8s:latest -t maciej55/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
+docker build -t maciej55/multi-worker-k8s:latest -t maciej55/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push maciej55/multi-client:latest
-docker push maciej55/multi-server:latest
-docker push maciej55/multi-worker:latest
+docker push maciej55/multi-client-k8s:latest
+docker push maciej55/multi-server-k8s:latest
+docker push maciej55/multi-worker-k8s:latest
 
-docker push maciej55/multi-client:$SHA
-docker push maciej55/multi-server:$SHA
-docker push maciej55/multi-worker:$SHA
+docker push maciej55/multi-client-k8s:$SHA
+docker push maciej55/multi-server-k8s:$SHA
+docker push maciej55/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-
-kubectl set image deployments/server-deployment server=maciej55/multi-server:$SHA
-kubectl set image deployments/client-deployment client=maciej55/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=maciej55/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=maciej55/multi-server-k8s:$SHA
+kubectl set image deployments/client-deployment client=maciej55/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=maciej55/multi-worker-k8s:$SHA
